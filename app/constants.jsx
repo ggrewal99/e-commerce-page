0e4407e24d84df25378c44f6a161405c754448cf
ggrewal99 <guruvindergrewal98@gmail.com>
2024-04-28 00:00:43 +1000

    Finish layout and styling of the product page

---
+++ app/constants.jsx
@@ -0,0 +1,20 @@
+const IMAGE_SHOWCASE = {
+    'img-1': {
+        full: '/assets/images/image-product-1.jpg',
+        thumbnail: '/assets/images/image-product-1-thumbnail.jpg',
+    },
+    'img-2': {
+        full: '/assets/images/image-product-2.jpg',
+        thumbnail: '/assets/images/image-product-2-thumbnail.jpg',
+    },
+    'img-3': {
+        full: '/assets/images/image-product-3.jpg',
+        thumbnail: '/assets/images/image-product-3-thumbnail.jpg',
+    },
+    'img-4': {
+        full: '/assets/images/image-product-4.jpg',
+        thumbnail: '/assets/images/image-product-4-thumbnail.jpg',
+    },
+};
+
+export default IMAGE_SHOWCASE;
--- app/page.js
+++ app/page.js
@@ -1,22 +1,93 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+"use client";
+import React, { useState } from 'react';
+import IMAGE_SHOWCASE from "./constants";
 
 function page() {
+    const [imageSelected, setImageSelected] = useState(IMAGE_SHOWCASE['img-1']['full']);
+    const [itemQuantity, setItemQuantity] = useState(0);
+
+    const handleThumbnailClick = (imageUrl) => {
+        setImageSelected(imageUrl);
+    };
+
+    const handleDecreaseQuantity = () => {
+        if (itemQuantity > 0) {
+            setItemQuantity(prevQuantity => prevQuantity - 1);
+        }
+    };
+
+    const handleIncreaseQuantity = () => {
+        setItemQuantity(prevQuantity => prevQuantity + 1);
+    };
     return (
-        <div className='mt-12 mx-2 lg:mx-4 flex'>
-            <div className="img-showcase-section">
-                <img src="/assets/images/image-product-1.jpg"
-                     alt=""
-                     className='rounded-xl'
-                />
+        <div className='mt-16 mx-2 lg:mx-12 flex gap-9'>
+            <div className="img-showcase-section flex-1 flex flex-col">
+                <div className='main-img'>
+                    <img src={imageSelected}
+                        alt=""
+                        className='rounded-xl w-[30rem] h-auto m-w-[20rem]
+                        cursor-pointer'
+                    />
+                </div>
+                <div className='other-img flex justify-between mt-8'>
+                    {Object.keys(IMAGE_SHOWCASE).map(key => (
+                        <img
+                            key={key}
+                            src={IMAGE_SHOWCASE[key]['thumbnail']}
+                            alt=""
+                            className={
+                                'w-[5rem] h-auto rounded-xl cursor-pointer hover:opacity-50 border-[3px] '
+                                + (imageSelected === IMAGE_SHOWCASE[key]['full'] ?
+                                    'border-primary-orange opacity-75' : 'border-transparent')
+                            }
+                            onClick={() => handleThumbnailClick(IMAGE_SHOWCASE[key]['full'])}
+                        />
+                    ))}
+                </div>
             </div>
-            <div className="text-section md:px-8 py-16">
+            <div className="text-section md:px-8 py-16 flex-1">
                 <h2
-                    className='uppercase font-extrabold text-primary-orange text-sm tracking-wider'>
+                    className='uppercase font-semibold text-primary-orange text-sm tracking-widest'>
                     Sneaker company
                 </h2>
                 <h1
-                className='text-3xl font-bold'>Fall Limited Edition Sneakers</h1>
+                    className='text-4xl font-bold break-words mt-6'>Fall Limited Edition Sneakers</h1>
+
+                <p className='text-neutral-darkGrayishBlue mt-10'>
+                    These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+                </p>
+
+                <div className='price flex flex-col mt-6'>
+                    <div className='flex items-center gap-5'>
+                        <h1 className='font-bold text-3xl'>$125.00</h1>
+                        <span className='bg-primary-paleOrange text-primary-orange px-2 py-1 rounded-md font-bold'>50%</span>
+                    </div>
+                    <h2 className='text-neutral-grayishBlue mt-2 font-bold line-through'>$250.00</h2>
+                </div>
+
+                <div className='purchase mt-6 flex gap-3'>
+                    <div className='qty flex items-center gap-3 bg-neutral-lightGrayishBlue 
+                    flex-0.5 rounded-lg p-3'>
+                        <button className='text-primary-orange font-bold text-2xl 
+                        transition ease-out duration-150 hover:opacity-50'
+                            onClick={handleDecreaseQuantity}>-</button>
+                        <span className='font-bold min-w-[4rem] text-center'>{itemQuantity}</span>
+                        <button className='text-primary-orange font-bold text-2xl 
+                        transition ease-out duration-150 hover:opacity-50'
+                            onClick={handleIncreaseQuantity}>+</button>
+                    </div>
+                    <div className='flex-1'>
+                        <button className='bg-primary-orange text-white w-full h-full 
+                        rounded-lg flex items-center justify-center hover:bg-opacity-50 transition ease-out duration-150'>
+                            <img
+                                src='/assets/icons/icon-cart.svg'
+                                alt=''
+                                className='cart-icon button me-2'
+                            />
+                            <span className='font-bold'>Add to Cart</span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
